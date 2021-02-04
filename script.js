b37fb7f2867b28bcec99cd6873e579370f00a656
Manjinder <manjindersinghjohal@aol.com>
2021-02-05 00:18:04 +0530

    refactor code

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 let total = 0;
 let basketItems = 0;
 
-const buyItemOne = () => {
+const totalElement = document.getElementById('total');
+const basketItemsElement = document.getElementById('basket-items');
+
+const buyItem = (price) => {
     basketItems++;
-    total += 20;
+    total += price;
 
-    document.getElementById('total').innerText = total;
-    document.getElementById('basket-items').innerText = basketItems;
-}
-
-const buyItemTwo = () => {
-    basketItems++;
-    total += 50;
-
-    document.getElementById('total').innerText = total;
-    document.getElementById('basket-items').innerText = basketItems;
+    totalElement.innerText = total;
+    basketItemsElement.innerText = basketItems;
 }
 
 const clearBasket = () => {
     basketItems = 0;
     total = 0;
 
-    document.getElementById('total').innerText = total;
-    document.getElementById('basket-items').innerText = basketItems;
+    totalElement.innerText = total;
+    basketItemsElement.innerText = basketItems;
 }
